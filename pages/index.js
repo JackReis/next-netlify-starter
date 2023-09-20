1a9a95ae5946574548811ae3e77c88ede730fcbe
Jack Reis <jackareis@gmail.com>
2023-09-19 20:45:15 -0700

    Customize text
    Hope it builds!

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="I, Jack Reis, am a for-hire Product Design Scientist"/>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Over the last 12 years, I’ve also held roles as a Design Technologist, System Educator, and Data Viz Lead.<br />
+          I blend practical considerations with a natural inclination toward beauty and simplicity to create valuable, intuitive, and enjoyable products.<br />
+          My most scaled project was at Google on the Nest smart home interfaces; Google holds more than 20% of the global smart speaker market and sold 50 million Google Home devices worldwide in 2021.<br />
+          While digital craft holds the majority of my attention, I am also a committed husband, trained yoga instructor, and lifelong learner.<br />
+          What fulfills me most is finding new, better ways to be and do things.<br />
         </p>
       </main>
 
