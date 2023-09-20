37362104badcde6e18a6e71cad708581afb4f09f
Jack Reis <jackareis@gmail.com>
2023-09-19 21:05:56 -0700

    Visualization

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="I, Jack Reis, am a for-hire Product Design Scientist"/>
         <p className="description">
-          Over the last 12 years, I’ve also held roles as a Design Technologist, System Educator, and Data Viz Lead.<br />
+          Over the last 12 years, I’ve also held roles as a Design Technologist, System Educator, and Data Visualization Lead.<br />
           I blend practical considerations with a natural inclination toward beauty and simplicity to create valuable, intuitive, and enjoyable products.<br />
           My most scaled project was at Google on the Nest smart home interfaces; Google holds more than 20% of the global smart speaker market and sold 50 million Google Home devices worldwide in 2021.<br />
           While digital craft holds the majority of my attention, I am also a committed husband, trained yoga instructor, and lifelong learner.<br />
